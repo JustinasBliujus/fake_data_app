331bceced8e13e2ccd5b42d3405f79fcb267db09
JustinasBliujus <45039478+JustinasBliujus@users.noreply.github.com>
2025-10-06 15:42:33 +0300

    Update generate_music_batch.js

--- routes/generator/generate_music_batch.js
+++ routes/generator/generate_music_batch.js
@@ -4,38 +4,22 @@ import { generateInfo } from './generate_info.js';
 import { generateMelody } from './generate_melody.js';
 import { combineSeedPage } from './combine_seed_page.js';
 import dotenv from 'dotenv';
-import fs from 'fs/promises';
-import path from 'path';
 
 dotenv.config();
 
 const BATCH_SIZE = parseInt(process.env.BATCH);
 
-export async function generateMusicBatch(seed, locale, size = BATCH_SIZE, pageNumber = 1) {
-    const coversDir = path.join(process.cwd(), 'public', 'covers');
-    try {
-        await fs.rm(coversDir, { recursive: true, force: true });
-        await fs.mkdir(coversDir, { recursive: true });
-    } catch (err) {
-        console.error(' Failed to clean /public/covers:', err);
-    }
-
+export async function generateMusicBatch(seed, locale, size = BATCH_SIZE, page) {
     const batch = [];
-    for (let i = 0; i < size; i++) {
-        const combinedSeed = combineSeedPage(seed, pageNumber, i, locale);
-        const fakerInstance = createFaker(combinedSeed, locale);
 
-        const info = await generateInfo(fakerInstance);
+    for (let i = 0; i < size; i++) {
+        const combinedSeed = combineSeedPage(seed, page, i, locale);
+        const fakerInstance = createFaker(combinedSeed, locale);
+        const info = await generateInfo(fakerInstance, seed, page);
         const lyrics = generateLyrics(fakerInstance);
         const music = generateMelody(fakerInstance);
 
-        batch.push({
-            seed: combinedSeed,
-            lyrics,
-            info,
-            music,
-            likes: 0,
-        });
+        batch.push({ combinedSeed, info, lyrics, music, likes: 0 });
     }
 
     return batch;
